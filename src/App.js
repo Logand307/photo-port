0206d964697c7e10bfdc0792c676c9044cc2a1e9
Logan Davis <Ldavis96@live.com>
2022-03-22 11:59:53 -0400

    added props to Nav component, finished Contact component error handling & tests, and ensured that App.js JSX was up to date

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Nav from "./components/Nav";
 import About from "./components/About";
-import Gallery from "./components/Gallery/index";
+import Gallery from "./components/Gallery";
 import ContactForm from "./components/Contact";
 
 function App() {
@@ -21,17 +21,26 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
